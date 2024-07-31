9f8420334a2fbc6d07a2bda2c15d72075c64157a
Sean Huang <huangtx2002@hotmail.com>
2024-07-31 18:58:33 +0000

    Enhanced for VS Code locally

--- tests/anchor.test.ts
+++ tests/anchor.test.ts
@@ -1,14 +1,25 @@
+import * as anchor from "@coral-xyz/anchor";
+import * as web3 from '@solana/web3.js';
+import { Program, web3 as anchorWeb3 } from '@project-serum/anchor';
+import { assert } from 'chai';
+import { AnchorCounter } from "../target/types/anchor_counter";
+
+// Configure the client to use the local cluster.
+const provider = anchor.AnchorProvider.env()
+anchor.setProvider(provider);
+const pg = anchor.workspace.AnchorCounter as Program<AnchorCounter>;
+
 describe("anchor-counter", () => {
   // Generate keypair for the new account
   const counterKp = new web3.Keypair();
 
   it("Is initialized!", async () => {
     // Send transaction
-    const txHash = await pg.program.methods
+    const txHash = await pg.methods
       .initialize()
       .accounts({
         counter: counterKp.publicKey,
-        user: pg.wallet.publicKey,
+        user: provider.wallet.publicKey,
         systemProgram: web3.SystemProgram.programId,
       })
       .signers([counterKp])
@@ -17,71 +28,71 @@ describe("anchor-counter", () => {
     console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
 
     // Confirm transaction
-    await pg.connection.confirmTransaction(txHash);
+    await provider.connection.confirmTransaction(txHash);
 
     // Fetch the created account
-    const initCounter = await pg.program.account.counter.fetch(
+    const initCounter = await pg.account.counter.fetch(
       counterKp.publicKey
     );
 
     console.log("On-chain data is:", initCounter.count.toString());
 
     // Check whether the data on-chain is equal to local 'counter'
-    const expectCount = new BN(0);
+    const expectCount = new anchor.BN(0);
     assert(expectCount.eq(initCounter.count));
   });
 
   it("Is incremented!", async () => {
     // Send transaction
-    const txHash = await pg.program.methods
+    const txHash = await pg.methods
       .increment()
       .accounts({
         counter: counterKp.publicKey,
-        user: pg.wallet.publicKey,
+        user: provider.wallet.publicKey,
       })
       .rpc();
 
     console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
 
     // Confirm transaction
-    await pg.connection.confirmTransaction(txHash);
+    await provider.connection.confirmTransaction(txHash);
 
     // Fetch the created account
-    const incrementCounter = await pg.program.account.counter.fetch(
+    const incrementCounter = await pg.account.counter.fetch(
       counterKp.publicKey
     );
 
     console.log("On-chain data is:", incrementCounter.count.toString());
 
     // Check whether the data on-chain is equal to local 'counter'
-    const expectCount = new BN(1);
+    const expectCount = new anchor.BN(1);
     assert(expectCount.eq(incrementCounter.count));
   });
 
   it("Is incremented again!", async () => {
     // Send transaction
-    const txHash = await pg.program.methods
+    const txHash = await pg.methods
       .increment()
       .accounts({
         counter: counterKp.publicKey,
-        user: pg.wallet.publicKey,
+        user: provider.wallet.publicKey,
       })
       .rpc();
 
     console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
 
     // Confirm transaction
-    await pg.connection.confirmTransaction(txHash);
+    await provider.connection.confirmTransaction(txHash);
 
     // Fetch the created account
-    const incrementCounter = await pg.program.account.counter.fetch(
+    const incrementCounter = await pg.account.counter.fetch(
       counterKp.publicKey
     );
 
     console.log("On-chain data is:", incrementCounter.count.toString());
 
     // Check whether the data on-chain is equal to local 'counter'
-    const expectCount = new BN(2);
+    const expectCount = new anchor.BN(2);
     assert(expectCount.eq(incrementCounter.count));
   });
 });
